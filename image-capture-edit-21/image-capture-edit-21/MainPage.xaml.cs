94963483d207e788cabc8d5ce4ef26d71dc5c80f
Tom Gregor <tgregor@ivsoftware.com>
2021-10-10 14:44:17 -0600

    Add project files.

---
+++ image-capture-edit-21/image-capture-edit-21/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.XamarinForms.ImageEditor;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace image_capture_edit_21
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        new MPBinding BindingContext => (MPBinding)base.BindingContext;
+
+    }
+    class MPBinding : INotifyPropertyChanged
+    {
+        public MPBinding()
+        {
+            InitImage();
+        }
+        private async void InitImage()
+        {
+            var file = await MediaPicker.CapturePhotoAsync();
+
+            if (file == null)
+            {
+                // Cancelled
+                return;
+            }
+            Image = file.FullPath;
+        }
+
+        ImageSource _Image = null;
+        public ImageSource Image
+        {
+            get => _Image;
+            set
+            {
+                if ((_Image == null) || !_Image.Equals(value))
+                {
+                    _Image = value;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public string FilePath { get; internal set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ image-capture-edit-21/image-capture-edit-21/MediaHelper.cs
@@ -0,0 +1,65 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.Threading.Tasks;
+
+namespace QSF.Examples.ImageEditorControl
+{
+    internal static class MediaHelper
+    {
+        public static bool CanTakePhoto
+        {
+            get
+            {
+                var mediaPlugin = CrossMedia.Current;
+
+                return mediaPlugin.IsTakePhotoSupported &&
+                       mediaPlugin.IsCameraAvailable;
+            }
+        }
+
+        public static bool CanPickPhoto
+        {
+            get
+            {
+                var mediaPlugin = CrossMedia.Current;
+
+                return mediaPlugin.IsPickPhotoSupported;
+            }
+        }
+
+        public static async Task<bool> InitializeAsync()
+        {
+            var mediaPlugin = CrossMedia.Current;
+
+            return await mediaPlugin.Initialize();
+        }
+
+        public static async Task<string> TakePhotoAsync()
+        {
+            if (!CanTakePhoto)
+            {
+                return null;
+            }
+
+            var mediaPlugin = CrossMedia.Current;
+            var mediaOptions = new StoreCameraMediaOptions();
+            var mediaFile = await mediaPlugin.TakePhotoAsync(mediaOptions);
+
+            return mediaFile?.Path;
+        }
+
+        public static async Task<string> PickPhotoAsync()
+        {
+            if (!CanPickPhoto)
+            {
+                return null;
+            }
+
+            var mediaPlugin = CrossMedia.Current;
+            var mediaOptions = new PickMediaOptions();
+            var mediaFile = await mediaPlugin.PickPhotoAsync(mediaOptions);
+
+            return mediaFile?.Path;
+        }
+    }
+}
